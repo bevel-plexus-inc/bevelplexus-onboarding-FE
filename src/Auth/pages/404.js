0a25a6d937c28fb64bbf768c1cc99455c7106842
treasurechic <babychic140@gmail.com>
2021-01-08 22:38:29 +0100

    update

--- src/Auth/pages/404.js
+++ src/Auth/pages/404.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import notfound from '../../assets/img/404.png'; 
-// import notfound from 'assets/img/404.png'; 
+// const notfound = require(`${process.env.REACT_APP_BASEURL}/assets/img/404.png`); 
+// import notfound from '../../assets/img/404.png'; 
 
 const Notfound = () => {
   return (
     <div className="notfound-wrapper">
       <div>
-        <img src={notfound} alt="" />
+        <img src={`${process.env.REACT_APP_BASEURL}/assets/img/404.png`} alt="" />
         <div className="back-home text-center mt-4">
           <Link to="/">
             <button className='btn btn-blue'>Back to home</button>
