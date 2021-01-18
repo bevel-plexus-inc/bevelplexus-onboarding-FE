da65183c7a3bf156dcc39eb8c8d37730bdac0327
treasurechic <babychic140@gmail.com>
2021-01-18 16:00:29 +0100

    update

--- src/Auth/pages/Register-step-four-regular.js
+++ src/Auth/pages/Register-step-four-regular.js
@@ -95,7 +95,7 @@ const RegisterStepFourRegular = () => {
             <div className="pt-space">
               <div className="d-flex flex-wrap align-items-end justify-content-end">
                 
-                  <Link to="/payment">
+                  <Link to="/dashboard">
                     <button className="btn btn-blue btn-lg" disabled={!enrollmentVerified}>Next</button>
                   </Link>
               </div>
